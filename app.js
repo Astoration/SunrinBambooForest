c1e6759b270543e446759b8afa085c3382b671b2
Astoration <ybdh00@naver.com>
2016-12-06 08:16:06 +0000

    inital base bot

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+const http = require('http')
+const Bot = require('messenger-bot')
+const process = require('process')
+
+let bot = new Bot({
+  token: 'EAALsYZB66QHsBAOwyDN2qeZAWA9nRY8WmJZBGRIZBz1YhtAZAwYYogERApZAxh23hXNvcngtSXSOlsCfiMAZCZAvZAaInRI1GQUTarNTAyy6IyZCpzROTbqyoqUdX35PcVEHm3E0hD3CSRtzCDOanZBEdZBsnZBRdr2FqcEizx9sdNRQDGQZDZD',
+  verify: 'helloworld',
+  app_secret: 'dac3c94ac910290780808f732281daa5'
+})
+
+bot.on('error', (err) => {
+  console.log(err.message)
+})
+
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+http.createServer(bot.middleware()).listen(process.env.PORT)
